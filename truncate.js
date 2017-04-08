6ed4e7b76bdc8eb8b7430bec88594ee4cf9dcb73
lewisj13 <lewisj13@seattleu.edu>
2017-04-08 13:42:26 -0700

    Updated js

--- truncate.js
+++ truncate.js
@@ -1,23 +1,44 @@
 // This is the file where you will write the Truncate Words function and related code.
 
 // TODO: Create a Function called truncateWords() that accepts two arguments: 
-// longText (a String with several words in it)
-// numWords (an Integer that sets the number of words you want in the returned text)
+          // longText (a String with several words in it)
+          // numWords (an Integer that sets the number of words you want in the returned text)
 
-// TODO: Within the truncateWords() Function, complete the following steps:
+
+// Function 
+var truncateWords = function(longText, numWords){
+      'use strict';
+    
 // 1. Use the split() function to split the String into an Array
+      var textArray = longText.split('');
+      console.log(textArray);
+      
 // 2. Use the length attribute to find the number of words in the Array
+      var removeWords =textArray.length - numWords;
+      console.log(removeWords);
+      
 // 3. Determine how many words should be removed from the String
 // 4. Remove those words from the Array
 // 5. Add an additional String item to the Array to put an ellipses in: "..."
+      textArray.splice(numWords,removeWords);
+      console.log(textArray);
+  
 // 6. Use the join() function to convert the Array back into a String
+      var truncString = textArray.join('');
+      console.log (textArray);
+  
 // 7. Return the truncated String from the Function
+      return truncString; 
 
 // This portion of the script is meant to call and display the result of your Function.
 // You do not need to change the following lines, but you may change them if you 
 // are experimenting further or pursuing stretch goals.
+  
+}
+
 var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 var wordLimit = 8;
 var shortText = truncateWords(originalText, wordLimit);
 console.log('originalText: ' + originalText);
 console.log('shortText: ' + shortText);
+
